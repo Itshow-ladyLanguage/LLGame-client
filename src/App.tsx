1cb0430b4c5b98ba14c4dfa19ce3aa4f7c3b1fdb
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-05-11 05:59:52 +0900

    route 연결, Login 페이지 div 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import { useState } from "react";
-function App() {
-	return <div></div>;
-}
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} /> {/* 로그인 페이지 */}
+    </Routes>
+  );
+};
 
 export default App;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '98vh'
+    }}>
+      <div style={{
+        backgroundColor: '#E10CA1',
+        padding: '20px',
+        borderRadius: '72px',
+        width: '892px',
+        height: '681px',
+        border: '3px solid #850E35'
+      }}>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
