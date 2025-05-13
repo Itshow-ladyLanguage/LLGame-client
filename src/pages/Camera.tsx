86cb49750a869282742d9edab86820079472e585
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-05-13 19:10:51 +0900

    웹캠 오버레이 수정, 블러 효과 적용

--- src/pages/Camera.tsx
+++ src/pages/Camera.tsx
@@ -1,10 +1,10 @@
-import React, { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
 import Webcam from "react-webcam";
 import "./Camera.css";
 
 const Camera = () => {
   const webcamRef = useRef(null);
-  const [time, setTime] = useState<number>(3);
+  const [time, setTime] = useState<number>(5);
 
   // 자신의 컴퓨터 카메라를 쓸 것
   const videoConstraints = {
@@ -23,6 +23,7 @@ const Camera = () => {
         justifyContent: "center",
         alignItems: "center",
         height: "98vh",
+        position: "relative",
       }}
     >
       <div
@@ -30,44 +31,79 @@ const Camera = () => {
           backgroundColor: "#E10CA1",
           padding: "20px",
           borderRadius: "72px",
-          width: "1100px",
-          height: "750px",
+          width: "1000px",
+          height: "650px",
           border: "3px solid #850E35",
           display: "flex",
           alignItems: "center", // 세로 중앙 정렬
           flexDirection: "column",
+          position: "relative",
         }}
       >
-        <Webcam
-          audio={false}
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          mirrored={true}
-          videoConstraints={videoConstraints}
-          style={{
-            width: "960px",
-            height: "580px",
-            borderRadius: "42px",
-            objectFit: "cover",
-            marginTop: "60px",
-          }}
-        />
+        <div style={{ position: "relative" }}>
+          <Webcam
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            mirrored={true}
+            videoConstraints={videoConstraints}
+            style={{
+              width: "860px",
+              height: "480px",
+              borderRadius: "42px",
+              objectFit: "cover",
+              marginTop: "60px",
+            }}
+          />
+          <div
+            className="overlay"
+            style={{
+              position: "absolute",
+              top: "60px",
+              left: 0,
+              width: "860px",
+              height: "480px",
+              borderRadius: "42px",
+              overflow: "hidden",
+              pointerEvents: "none" /* 마우스 터치 이벤트 무시 (이 코드가 없으면 클릭이 오버레이에서 먹혀서 버튼이나 웹캠 캡처가 안 될수도 있음) */,
+              zIndex: 2,
+            }}
+          >
+            <div
+              className="circle-camera"
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                width: "400px",
+                height: "400px",
+                transform: "translate(-50%, -50%)",
+                borderRadius: "50%",
+                boxShadow: "0 0 0 310px rgba(0, 0, 0, 0.5)",
+                mixBlendMode: "normal",
+              }}
+            />
+          </div>
+        </div>
+
         {time > 0 && (
           <div
             style={{
               position: "absolute",
-              top: "50%",
+              top: "48%",
               left: "50%",
               transform: "translate(-50%, -50%)",
               fontSize: "150px",
-              color: "#FFCEF0",
+              color: "#E10CA1",
               fontWeight: "bold",
               fontFamily: "'Pretendard-Regular', sans-serif",
+              zIndex: 3,
             }}
           >
             {time}
           </div>
         )}
+
         <div className="btn-container">
           <button
             type="submit"
