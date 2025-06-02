fd4cc7c333e16baec9cb9c4c99bf951fb8884635
Anchaeyeon <s2328@e-mirim.hs.kr>
2025-06-03 01:41:59 +0900

    결과 페이지 화면 캡처 기능 구현

--- src/pages/ResultPages.tsx
+++ src/pages/ResultPages.tsx
@@ -1,9 +1,19 @@
 import { useEffect } from "react";
 import Result from "../components/Result";
+import html2canvas from "html2canvas";
 
 export default function ResultPages() {
   useEffect(() => {
     document.body.style.margin = "0";
+
+    const capture = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 500)); // 화면 렌더링이 되기 전까지 0.5초 대기
+      const canvas = await html2canvas(document.body); // 화면 전체 캡쳐
+      const imgUrl = canvas.toDataURL("image/png");
+
+      console.log("캡쳐된 이미지:", imgUrl);
+    };
+    capture();
   }, []);
 
   return (
